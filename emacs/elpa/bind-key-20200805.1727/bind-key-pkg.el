98ec30bd21bdb616807dab688b10781b95812747
vibikim <vib@vibbe.xyz>
2021-02-19 10:56:25 +0200

    emacs gitignore ig

--- emacs/elpa/bind-key-20200805.1727/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/vibikim/.config/emacs/elpa/bind-key-20200805.1727/bind-key.el  -*- no-byte-compile: t -*-
-(define-package "bind-key" "20200805.1727" "A simple way to manage personal keybindings" 'nil :commit "b79c25d09babc67315577b555701324ab2f15eb6" :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :keywords '("keys" "keybinding" "config" "dotemacs") :url "https://github.com/jwiegley/use-package")
